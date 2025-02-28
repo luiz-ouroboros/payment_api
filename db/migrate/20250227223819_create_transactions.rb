2ab38dbdee635206f0d062af5e40794514539875
Rufi Dev <luiz@ouroboros.dev.br>
2025-02-28 09:29:07 -0300

    Update CreateTransactions migration to add gateway

--- db/migrate/20250227223819_create_transactions.rb
+++ db/migrate/20250227223819_create_transactions.rb
@@ -5,8 +5,11 @@ class CreateTransactions < ActiveRecord::Migration[8.0]
       t.integer :installment, null: false
       t.string :payment_method, null: false
       t.string :status, null: false
+
       t.datetime :approved_at
       t.datetime :reproved_at
+      t.string :gateway, null: false
+
       t.timestamps
     end
   end
